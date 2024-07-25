77c63fec6a1a6150ae4b3837f062619ef24ff9a3
Zayyan <87618873+zayyanr@users.noreply.github.com>
2024-07-24 22:34:22 -0400

    Create background.js

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.webNavigation.onCompleted.addListener((details) => {
+  chrome.tabs.sendMessage(details.tabId, { action: "checkForAds" });
+}, { url: [{ urlMatches: 'https://www.youtube.com/*' }] });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // Check if the URL contains "googleads.g.doubleclick.net" or "doubleclick.net"
+    if (details.url.includes("googleads.g.doubleclick.net") || details.url.includes("doubleclick.net") || details.url.includes("googleusercontent.com")) {
+      console.log("Blocking URL: " + details.url);
+      return { cancel: true };
+    }
+    return { cancel: false };
+  },
+  { urls: ["*://*.doubleclick.net/*", "*://*.googleads.g.doubleclick.net/*", "*://*.googleusercontent.com/*"] }, // Match all subdomains and paths
+  ["blocking"]
+);
