bc62e9bfd725c0b7c1d078977a520c4bec92e776
Zayyan <87618873+zayyanr@users.noreply.github.com>
2024-07-24 22:29:55 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.webNavigation.onCompleted.addListener((details) => {
   chrome.tabs.sendMessage(details.tabId, { action: "checkForAds" });
 }, { url: [{ urlMatches: 'https://www.youtube.com/*' }] });
 
-chrome.webRequest.onBeforeRequest.addListener(
+/* chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     // Check if the URL contains "googleads.g.doubleclick.net" or "doubleclick.net"
     if (details.url.includes("googleads.g.doubleclick.net") || details.url.includes("doubleclick.net") || details.url.includes("googleusercontent.com")) {
@@ -13,4 +13,6 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   { urls: ["*://*.doubleclick.net/*", "*://*.googleads.g.doubleclick.net/*", "*://*.googleusercontent.com/*"] }, // Match all subdomains and paths
   ["blocking"]
-);
+); */
+
+// UNCOMMENT TO BLOCK REQUESTS OF ADS SINCE ANTI-VIRUS IS TRIGGERED.
